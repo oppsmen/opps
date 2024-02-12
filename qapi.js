b7f62f29281bf8e9f1cba5e847a6071491c5d230
oppsmen <159833698+oppsmen@users.noreply.github.com>
2024-02-12 22:09:31 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"65ca88560072400020e9fabe",
+				"65ca88560072400020e9fac1"
+			]
+		},
+		"65ca88560072400020e9fabe": {
+			"id": "65ca88560072400020e9fabe",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"65ca88560072400020e9fac1": {
+			"id": "65ca88560072400020e9fac1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
